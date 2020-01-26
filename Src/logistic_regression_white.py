103021ef48f3900bc979295db7647819d0bb5109
Michal <michalkedzierski96@gmail.com>
2020-01-26 20:46:32 +0100

    Update

--- Src/logistic_regression_white.py
+++ Src/logistic_regression_white.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 #Read the data file
 data=pd.read_csv('../Data/winequality-white.csv')
-red_data_training, red_data_test, red_quality_training, red_quality_test,white_data_training,white_data_test, white_quality_training, white_quality_test = read_data(True)
+red_data_training, red_data_test, red_quality_training, red_quality_test,white_data_training,white_data_test, white_quality_training, white_quality_test = read_data(False)
 
 reg = LogisticRegression()
 reg.fit(white_data_training, white_quality_training)
